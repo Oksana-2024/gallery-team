24a790fb6fc63a9c0f31eb9818846af6a4125047
Sasha Pavluk <bogota3443@gmail.com>
2024-11-10 16:29:51 +0200

    loader

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,11 +1,14 @@
 import { getPhotos } from './unsplash-api';
 import { refs } from './refs';
 import { createMarkup } from './createmarkup';
+import { showLoader, hideLoader } from './loader';
 
 refs.form.addEventListener('submit', onSubmit);
 let page = 1;
 async function onSubmit(event) {
   event.preventDefault();
+  refs.list.innerHTML = '';
+  showLoader();
 
   const searchQuery = event.target.elements['user-search-query'].value.trim();
 
@@ -14,5 +17,8 @@ async function onSubmit(event) {
     refs.list.innerHTML = createMarkup(responce.results);
   } catch (error) {
     console.log(error);
+  } finally {
+    hideLoader();
   }
 }
+console.log(refs.loader);
